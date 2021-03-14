6d73f7385c003bc590b3ca03e66082cc180ed112
mutoe <mutoe@foxmail.com>
2021-03-14 14:46:12 +0800

    fix(test): fix import.meta transform issue

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,32 @@
 // Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
 
-module.exports = api => {
+const convertImportMetaEnv = () => ({
+  visitor: {
+    MetaProperty (path) {
+      path.replaceWithSourceString('process')
+    },
+  },
+})
+
+module.exports = (api) => {
   const isTest = api.env('test')
 
   return {
     presets: [
-      ['@babel/preset-env', { modules: isTest ? 'auto' : false }],
+      ['@babel/preset-env', isTest
+        ? {
+            useBuiltIns: 'entry',
+            corejs: '2',
+            targets: { node: 'current' },
+          }
+        : { modules: false }],
       '@babel/preset-typescript',
       '@babel/preset-react',
     ],
     plugins: [
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-transform-runtime',
+      isTest && convertImportMetaEnv,
     ],
   }
 }
