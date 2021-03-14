cdba64ed847e15b4d42adeea188302dedd5afa9a
mutoe <mutoe@foxmail.com>
2021-03-14 12:46:24 +0800

    fix: the API prefix issue

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: String(import.meta.env.VITE_API_URL ?? 'http://localhost:8080'),
+}
+
+export default CONFIG
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,14 @@
+import CONFIG from 'src/config'
 import { Api } from './api'
 
 export * from './hooks/useSubmit'
 export * from './hooks/useRetrieveList'
 
 export const service = new Api<string>({
+  baseURL: CONFIG.API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   securityWorker: token => token
     ? {
         headers: {
