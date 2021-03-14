5663cca6e25c810c074fbaa3b134b4cc6c031846
mutoe <mutoe@foxmail.com>
2021-03-14 12:38:16 +0800

    fix(build): semantic css build issue

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+import 'semantic-ui-css/semantic.min.css'
 import 'draft-js/dist/Draft.css'
 import 'src/assets/css/index.scss'
 
