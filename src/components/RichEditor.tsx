86043ec211bb60748b41dda49c53eb2a2ba99715
mutoe <mutoe@foxmail.com>
2021-03-26 00:29:18 +0800

    feat(article): add content rich editor when creating article

--- src/components/RichEditor.tsx
+++ src/components/RichEditor.tsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import { Editor, EditorState } from 'draft-js'
+import './RichEditor.scss'
 
-const RichEditor: React.FC = () => {
+interface RichEditorProps {
+  name: string
+  label: string
+  placeholder?: string
+  required?: boolean
+  disabled?: boolean
+}
+
+const RichEditor: React.FC<RichEditorProps> = (props) => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty())
 
   useEffect(() => {
     console.log(editorState)
   }, [editorState])
-  return <Editor editorState={editorState} onChange={setEditorState} />
+
+  return <div className="field">
+    <label>{props.label}</label>
+    <div className="ui rich-editor">
+      <Editor editorState={editorState} onChange={setEditorState} />
+    </div>
+  </div>
 }
 
 export default RichEditor
