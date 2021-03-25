64d764a53c422477a064f6899e91040cb97dcfb2
mutoe <mutoe@foxmail.com>
2021-03-26 00:20:36 +0800

    feat(article): distinguish between creation and editing

--- src/pages/content/article/ArticleEditPage.tsx
+++ src/pages/content/article/ArticleEditPage.tsx
@@ -1,32 +1,41 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Menu, Segment } from 'semantic-ui-react'
+import { Header, Icon, Menu, Segment } from 'semantic-ui-react'
 import FormRenderer, { FieldConfig } from 'src/components/FormRenderer'
+
+const form = {
+  title: '',
+  content: '',
+}
+
+const formConfig: FieldConfig<keyof typeof form>[] = [
+  { type: 'input', name: 'title', label: 'Title' },
+  { type: 'rich', name: 'content', label: 'Content' },
+]
 
 const ArticleEditPage: React.FC = () => {
   const { id } = useParams()
-  // TODO: Create article feature
-  const isCreate = id === 'create'
   const navigate = useNavigate()
 
-  const form = {
-    title: '',
-    content: '',
-  }
+  const isCreate = id === 'create'
 
-  const formConfig: FieldConfig<keyof typeof form>[] = [
-    { type: 'input', name: 'title', label: 'Title' },
-    { type: 'rich', name: 'content', label: 'Content' },
-  ]
-
-  return <div>
+  return <>
     <Menu attached='top'>
       <Menu.Item role='link' icon='angle left' content='Back' onClick={() => navigate(-1)} />
     </Menu>
+
     <Segment attached='bottom'>
+      <Header as="h2" >
+        <Icon name="edit" />
+        <Header.Content>
+          {isCreate ? 'Create ' : 'Edit '}
+          Article
+        </Header.Content>
+      </Header>
+
       <FormRenderer initForm={form} fields={formConfig} />
     </Segment>
-  </div>
+  </>
 }
 
 export default ArticleEditPage
